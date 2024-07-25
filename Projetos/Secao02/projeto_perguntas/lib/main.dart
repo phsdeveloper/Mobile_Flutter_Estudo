f7f9e24f9d0b8cd00081b61eab9160d9919ab8ee
Paulo Santos <phs.developer@gmail.com>
2024-07-24 21:25:20 -0300

    Aula finalizada com sucesso - Interessante

--- Projetos/Secao02/projeto_perguntas/lib/main.dart
+++ Projetos/Secao02/projeto_perguntas/lib/main.dart
@@ -2,27 +2,30 @@ import 'package:flutter/material.dart';
 
 main()=> runApp(PerguntaApp());
 
-class PerguntaApp extends StatelessWidget {
-  const PerguntaApp({super.key});
+class PerguntaAppState extends State <PerguntaApp>{
 
-  @override
-  Widget build(BuildContext context) {
-  final List<String> perguntas = [
-    '01) Qual é a sua cor favorita',
-    '02) Qual é a Seu animal',
-    '03) Qual é a sua Comida favorita',
-  ];
 
   int perguntaSelecionada = 0;
-
+final List<String> perguntas = [
+    '01) Qual é a sua cor favorita',
+    '02) Qual é a Seu animal favorito',
+    '03) Qual é a sua Comida favorita',
+    '04) O Samuel é lindo?',
+  ];
   void Responder(){
-    perguntaSelecionada++;
+    setState(() {
+      perguntaSelecionada++;
+    });
+    
     if(perguntaSelecionada>=perguntas.length)
     {
       perguntaSelecionada = 0;
     }
     print('pergunta "${perguntas[perguntaSelecionada]}" Respondida.');
   }
+
+    @override
+  Widget build(BuildContext context) {
 
     return MaterialApp(
       home: Scaffold(
@@ -33,13 +36,28 @@ class PerguntaApp extends StatelessWidget {
         body: Column(
           children: [
              Text(perguntas[perguntaSelecionada]),
-             ElevatedButton(child: Text('Resposta 01'),onPressed: Responder),
-             ElevatedButton(child: Text('Resposta 02'),onPressed: (){print('teste');}),
-             ElevatedButton(child: const Text('Resposta 03'),onPressed: () => print('Resposta 3!!!'),),
-             ElevatedButton(child: const Text('Resposta 04 - Desabilitado'),onPressed: null,),
+             ElevatedButton(child: Text('Com certeza'),onPressed: Responder),
+             ElevatedButton(child: Text('Não Acho'),onPressed: (){print('teste');}),
+             ElevatedButton(child: const Text('Mais ou menos'),onPressed: () => print('Resposta 3!!!'),),
+             ElevatedButton(child: const Text('Não vou responder - Desabilitado'),onPressed: null,),
           ],
         ),
       )
     );
   }
+}
+
+
+
+
+
+
+class PerguntaApp extends StatefulWidget {
+  //const PerguntaApp({super.key});
+  @override
+  PerguntaAppState createState() {
+    return new PerguntaAppState();
+  }
+
+
 }
