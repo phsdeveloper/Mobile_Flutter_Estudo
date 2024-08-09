1fc989b24f00473ade778ea61cb4d9090b88aeb4
Paulo Santos <phs.developer@gmail.com>
2024-08-09 16:47:22 -0300

    Aula finalizada

--- Projetos/Secao02/projeto_perguntas/lib/resultado.dart
+++ Projetos/Secao02/projeto_perguntas/lib/resultado.dart
@@ -6,7 +6,7 @@ class Resultado extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: Text('Parabéns',style: TextStyle(fontSize: 28),),
+      child: Text('Parabéns!!',style: TextStyle(fontSize: 28),),
     );
   }
 }
