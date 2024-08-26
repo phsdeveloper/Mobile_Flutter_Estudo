d95b72876ddca6820b9a5b5f278e62151c94bf45
Paulo Santos <phs.developer@gmail.com>
2024-08-26 20:51:27 -0300

    Aula 93. Finalizada com sucesso e aplicacao compilando

--- Projetos/Secao03/expenses/lib/main.dart
+++ Projetos/Secao03/expenses/lib/main.dart
@@ -54,13 +54,18 @@ class MyHomePage extends StatelessWidget {
                       ),
                       decoration: BoxDecoration(
                         border: Border.all(
-                          color: Colors.black,
+                          color: Colors.purple,
                           width: 2,
                         )
                       ),
                       padding: const EdgeInsets.all(10),
                       child: Text(
-                        tr.value.toString()
+                        tr.value.toString(),
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.purple
+                        ),
                       ),
                     ),
                     Column(
